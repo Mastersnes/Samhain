8fc6f7f8fe86f4e3f56ef6aa47940e7409d60827
NESZTLER Steven Ext DTSI/DIF <XLGD4259@orange.com>
2020-11-20 16:15:13 +0100

    Fin de la traduction EN !!!!!!

--- WebContent/app/data/textes/stories/tuto/crypte/salle2-textes.js
+++ WebContent/app/data/textes/stories/tuto/crypte/salle2-textes.js
@@ -2,168 +2,207 @@
 define([], function(){
 	var data = {
         "necroS2-texte-1" : {
-            fr : "Vous ouvrez la porte et entrez doucement dans la pièce.",
-            en : "You open the door and enter the room slowly."
+        	fr : "Vous ouvrez la porte et entrez doucement dans la pièce.",
+        	en : "You open the door and enter the room slowly.",
+        	eo : ""
         },
         "necroS2-texte-2" : {
-            fr : "Les cris se font de plus en plus pressants puis s'arrêtent d'un coup.",
-            en : "The screams are getting louder and louder and stop suddenly."
+        	fr : "Les cris se font de plus en plus pressants puis s'arrêtent d'un coup.",
+        	en : "The screams become louder and louder then stop suddenly.",
+        	eo : ""
         },
         "necroS2-texte-3" : {
-            fr : "Vous apercevez une ombre dans le fond de la pièce.",
-            en : "You see a shadow in the background of the room."
+        	fr : "Vous apercevez une ombre dans le fond de la pièce.",
+        	en : "You glimpse a shadow in the back of the room.",
+        	eo : ""
         },
         "necroS2-texte-4" : {
-            fr : "Elle vous parle d'une voix enfantine :",
-            en : "She speaks to you in a childish voice:"
+        	fr : "Elle vous parle d'une voix enfantine&nbsp;:",
+        	en : "It speaks to you in a childish voice:",
+        	eo : ""
         },
         "necroS2-texte-5" : {
-            fr : "&laquo;&nbsp;Qui est là&nbsp;? J'ai peur&hellip; J'ai faim&hellip;&nbsp;&raquo;",
-            en : "&laquo;&nbsp;Who's out there? I'm scared&hellip; I'm hungry&hellip;&nbsp;&raquo;"
+        	fr : "&laquo;&nbsp;Qui est là&nbsp;? J'ai peur&hellip; J'ai faim&hellip;&nbsp;&raquo;",
+        	en : "&laquo;&nbsp;Who's here? I'm scared&hellip; I'm hungry&hellip;&nbsp;&raquo;",
+        	eo : ""
         },
 
         "necroS2Retour-texte-1" : {
-            fr : "L'ombre dans le fond de la pièce vous regarde toujours sans bouger.",
-            en : "The shadow in the background of the room always looks at you without moving."
+        	fr : "L'ombre dans le fond de la pièce vous regarde toujours sans bouger.",
+        	en : "The shadow in the back of the room still looks at you without moving.",
+        	eo : ""
         },
         "necroS2Retour-texte-2" : {
-            fr : "Elle vous parle d'une voix enfantine, calmement :",
-            en : "She speaks to you in a childlike voice, calmly:"
+        	fr : "Elle vous parle d'une voix enfantine, calmement&nbsp;:",
+        	en : "It speaks to you in a childish voice, calmly:",
+        	eo : ""
         },
         "necroS2Retour-texte-3" : {
-            fr : "&laquo;&nbsp;Te revoilà&nbsp;? Tu viens jouer&nbsp;?&nbsp;&raquo;",
-            en : "&laquo;&nbsp;Are you back again? Are you coming to play?&nbsp;&raquo;"
+        	fr : "&laquo;&nbsp;Te revoilà&nbsp;? Tu viens jouer&nbsp;?&nbsp;&raquo;",
+        	en : "&laquo;&nbsp;Are you back? Are you coming to play?&nbsp;&raquo;",
+        	eo : ""
         },
 
         "necroS22-texte-1" : {
-            fr : "Vous vous approchez tout doucement pour ne pas l'effrayer&hellip;",
-            en : "You approach slowly so as not to frighten him&hellip;"
+        	fr : "Vous vous approchez tout doucement pour ne pas l'effrayer&hellip;",
+        	en : "You approach very slowly as not to frighten him&hellip;",
+        	eo : ""
         },
         "necroS22-texte-2" : {
-            fr : "La silhouette s'enfonce un peu plus dans l'obscurité.",
-            en : "The silhouette sinks a little deeper into the darkness."
+        	fr : "La silhouette s'enfonce un peu plus dans l'obscurité.",
+        	en : "The silhouette sinks a little deeper into the darkness.",
+        	eo : ""
         },
         "necroS22-texte-3" : {
-            fr : "Vous entendez soudain une voix criarde :",
-            en : "You suddenly hear a screaming voice:"
+        	fr : "Vous entendez soudain une voix criarde&nbsp;:",
+        	en : "You suddenly hear a screaming voice:",
+        	eo : ""
         },
         "necroS22-texte-4" : {
-            fr : "&laquo;&nbsp;C'est un méchant&nbsp;! Tue-le&nbsp;! Tue-le, déchet&nbsp;!&nbsp;&raquo;",
-            en : "&laquo;&nbsp;He's a bad guy! Kill him! Kill him, you trash!&nbsp;&raquo;"
+        	fr : "&laquo;&nbsp;C'est un méchant&nbsp;! Tue-le&nbsp;! Tue-le, déchet&nbsp;!&nbsp;&raquo;",
+        	en : "&laquo;&nbsp;It's a bad guy! Kill him! Kill him, damned trash!&nbsp;&raquo;",
+        	eo : ""
         },
         "necroS22-texte-5" : {
-            fr : "Puis, la voix enfantine :",
-            en : "Then the childish voice:"
+        	fr : "Puis, la voix enfantine&nbsp;:",
+        	en : "Then the childish voice:",
+        	eo : ""
         },
         "necroS22-texte-6" : {
-            fr : "&laquo;&nbsp;Non&hellip; Il ne faut pas&hellip;&nbsp;&raquo;",
-            en : "&laquo;&nbsp;No&hellip; You mustn' t&hellip;&nbsp;&raquo;"
+        	fr : "&laquo;&nbsp;Non&hellip; Il ne faut pas&hellip;&nbsp;&raquo;",
+        	en : "&laquo;&nbsp;No&hellip; It’s forbidden&hellip;&nbsp;&raquo;",
+        	eo : ""
         },
 
         "necroS2Parle-texte-1" : {
-            fr : "Vous essayez de rassurer l'enfant d'une voix douce.",
-            en : "You are trying to reassure the child with a soft voice."
+        	fr : "Vous essayez de rassurer l'enfant d'une voix douce.",
+        	en : "You try to reassure the child with a soft voice.",
+        	eo : ""
         },
         "necroS2Parle-texte-2" : {
-            fr : "Mais, c'est à nouveau la voix criarde qui vous répond :",
-            en : "But it is again the screaming voice that responds to you:"
+        	fr : "Mais, c'est à nouveau la voix criarde qui vous répond&nbsp;:",
+        	en : "But it’s again the screaming voice responding to you:",
+        	eo : ""
         },
         "necroS2Parle-texte-3" : {
-            fr : "&laquo;&nbsp;Silence&nbsp;! Insecte inutile&nbsp;! Il ne t'écoutera pas&nbsp;!",
-            en : "&laquo;&nbsp;Quiet! Insect useless! He won't listen to you!"
+        	fr : "&laquo;&nbsp;Silence&nbsp;! Insecte inutile&nbsp;! Il ne t'écoutera pas&nbsp;!",
+        	en : "&laquo;&nbsp;Silence! Useless bug! He won't listen to you!",
+        	eo : ""
         },
         "necroS2Parle-texte-4" : {
-            fr : "Maintenant, tue-le, où tu souffriras de nouveau&nbsp;!&nbsp;&raquo;",
-            en : "Now kill him or you'll suffer again!&nbsp;&raquo;"
+        	fr : "Maintenant, tue-le, où tu souffriras de nouveau&nbsp;!&nbsp;&raquo;",
+        	en : "Now kill him or you'll suffer again!&nbsp;&raquo;",
+        	eo : ""
         },
         "necroS2Parle-texte-5" : {
-            fr : "Vous voyez dans le noir deux points brillants&hellip;",
-            en : "You see in the dark two bright spots&hellip;"
+        	fr : "Vous voyez dans le noir deux points brillants&hellip;",
+        	en : "You see in the dark, two bright spots&hellip;",
+        	eo : ""
         },
 
         "necroS2Approche-texte-1" : {
-            fr : "L'ombre vous regarde fixement et s'avance doucement vers vous.",
-            en : "The shadow stares at you and slowly advances towards you."
+        	fr : "L'ombre vous regarde fixement et s'avance doucement vers vous.",
+        	en : "The shadow stares at you and slowly heads for you.",
+        	eo : ""
         },
         "necroS2Approche-texte-2" : {
-            fr : "La porte derrière vous claque violemment et vous entendez l'enfant rire&hellip;",
-            en : "The back door slams you hard and you hear the child laughing&hellip;"
+        	fr : "La porte derrière vous claque violemment et vous entendez l'enfant rire&hellip;",
+        	en : "The door behind you violently slams and you hear the child laughing&hellip;",
+        	eo : ""
         },
         "necroS2Approche-texte-3" : {
-            fr : "&laquo;&nbsp;Tu aurais dû t'enfuir tant que tu le pouvais&hellip;&nbsp;&raquo;",
-            en : "&laquo;&nbsp;You should have run away while you could&hellip;&nbsp;&raquo;"
+        	fr : "&laquo;&nbsp;Tu aurais dû t'enfuir tant que tu le pouvais&hellip;&nbsp;&raquo;",
+        	en : "&laquo;&nbsp;You had should run away while you could&hellip;&nbsp;&raquo;",
+        	eo : ""
         },
         "necroS2Approche-texte-4" : {
-            fr : "À mesure que la forme s'approche, vous la discernez mieux&hellip;",
-            en : "As the shape approaches, you can see it better&hellip;"
+        	fr : "À mesure que la forme s'approche, vous la discernez mieux&hellip;",
+        	en : "As the shape approaches, you can see it better&hellip;",
+        	eo : ""
         },
 
         "necroS2Enfant-texte-1" : {
-            fr : "Un enfant s'avance lentement, blanc comme un linge.",
-            en : "A child advances slowly, white as a sheet."
+        	fr : "Un enfant s'avance lentement, blanc comme un linge.",
+        	en : "A child come forward slowly, white as a sheet.",
+        	eo : ""
         },
         "necroS2Enfant-texte-2" : {
-            fr : "Ses bras sont anormalement longs et couverts de coupures.",
-            en : "His arms are abnormally long and covered with cuts."
+        	fr : "Ses bras sont anormalement longs et couverts de coupures.",
+        	en : "His arms are abnormally long and covered with cuts.",
+        	eo : ""
         },
         "necroS2Enfant-texte-3" : {
-            fr : "De ses doigts sortent de grandes tiges de fer&hellip;",
-            en : "From his fingers come out large rods of iron&hellip;"
+        	fr : "De ses doigts sortent de grandes tiges de fer&hellip;",
+        	en : "From his fingers come out large rods of iron&hellip;",
+        	eo : ""
         },
         "necroS2Enfant-texte-4" : {
-            fr : "En avançant, il se met à ricaner.",
-            en : "As he moves on, he starts giggling."
+        	fr : "En avançant, il se met à ricaner.",
+        	en : "As he come closer, he starts snickering.",
+        	eo : ""
         },
         "necroS2Enfant-texte-5" : {
-            fr : "Ses dents, pointues comme les crocs d'un loup, brillent d'une couleur rougeâtre.",
-            en : "Its teeth, pointed like a wolf's fangs, shine red."
+        	fr : "Ses dents, pointues comme les crocs d'un loup, brillent d'une couleur rougeâtre.",
+        	en : "His teeth, sharped like a wolf's fangs, shine red.",
+        	eo : ""
         },
 
         "necroS2Enfant2-texte-1" : {
-            fr : "Mais quelle était cette horreur&nbsp;?!",
-            en : "But what was that horror?!"
+        	fr : "Mais quelle était cette horreur&nbsp;?!",
+        	en : "But what was this abomination?!",
+        	eo : ""
         },
         "necroS2Enfant2-texte-2" : {
-            fr : "Les enfants capturés servaient à fabriquer ce monstre&nbsp;?!",
-            en : "The captured children were used to make this monster?!"
+        	fr : "Les enfants capturés servaient à fabriquer ce monstre&nbsp;?!",
+        	en : "The captured children were used to make this monster?!",
+        	eo : ""
         },
         "necroS2Enfant2-texte-3" : {
-            fr : "Vous réalisez soudain que d'autres enfants doivent subir le même sort à l'heure qu'il est&nbsp;!",
-            en : "Suddenly you realize that other children have to suffer the same fate now!"
+        	fr : "Vous réalisez soudain que d'autres enfants doivent subir le même sort à l'heure qu'il est&nbsp;!",
+        	en : "Suddenly, you realize that other children must endure the same fate now!",
+        	eo : ""
         },
         "necroS2Enfant2-texte-4" : {
-            fr : "Il n'est peut-être pas trop tard&nbsp;!",
-            en : "It may not be too late!"
+        	fr : "Il n'est peut-être pas trop tard&nbsp;!",
+        	en : "It may not be too late!",
+        	eo : ""
         },
         "necroS2Enfant2-texte-5" : {
-            fr : "Vous reprenez vos esprits et repartez à l'aventure.",
-            en : "You come to your senses and go back to adventure."
+        	fr : "Vous reprenez vos esprits et repartez à l'aventure.",
+        	en : "You take your breath and go back to adventure.",
+        	eo : ""
         },
 
         "necroS2Fouille-texte-1" : {
-            fr : "La salle se compose simplement d'une étagère remplie de vieux parchemins.",
-            en : "The room is simply a shelf filled with old scrolls."
+        	fr : "La salle se compose simplement d'une étagère remplie de vieux parchemins.",
+        	en : "The room is simply composed of a shelf filled with old scrolls.",
+        	eo : ""
         },
         "necroS2Fouille-texte-2" : {
-            fr : "En en lisant quelques-uns, vous découvrez qu'il est possible, avec la formule adéquate",
-            en : "By reading a few of them, you will discover that it is possible, with the appropriate formula"
+        	fr : "En en lisant quelques-uns, vous découvrez qu'il est possible, avec la formule adéquate",
+        	en : "By reading a few of them, you discover that it’s possible, with the appropriate formula",
+        	eo : ""
         },
         "necroS2Fouille-texte-3" : {
-            fr : "et un peu de concentration, de lancer une terrible <span key='bouleFeu'>boule de feu</span>&nbsp;!",
-            en : "and a little concentration, to throw a terrible <span key='bouleFeu'>fireball</span>!"
+        	fr : "et un peu de concentration, de lancer une terrible <span key='bouleFeu'>boule de feu</span>&nbsp;!",
+        	en : "and a little concentration, to throw a terrible <span key='bouleFeu'>fireball</span>!",
+        	eo : ""
         },
         "necroS2Fouille-texte-4" : {
-            fr : "Il est temps de retourner dans la grande salle&nbsp;! L'aventure n'attend pas&nbsp;!",
-            en : "It's time to go back to the big room! The adventure does not wait!"
+        	fr : "Il est temps de retourner dans la grande salle&nbsp;! L'aventure n'attend pas&nbsp;!",
+        	en : "It's time to go back to the big room! The adventure doesn’t wait!",
+        	eo : ""
         },
 
         "necroS2Clean-texte-1" : {
-            fr : "La salle est vide&hellip; Seule reste au sol cette <span key='experience'>expérience</span> ratée.",
-            en : "The room is empty&hellip; Only this failed <span key='experience'>experiment</span> remains on the ground."
+        	fr : "La salle est vide&hellip; Seule reste au sol cette <span key='experience'>expérience</span> ratée.",
+        	en : "The room is empty&hellip; Only this missed <span key='experience'>experiment</span> remains on the ground.",
+        	eo : ""
         },
         "necroS2Clean-texte-2" : {
-            fr : "Vous vous demandez quel être immonde a pu opérer telle transformation&hellip;",
-            en : "You're wondering what a disgusting thing it is to be could have done such a transformation&hellip;"
+        	fr : "Vous vous demandez quel être immonde a pu opérer telle transformation&hellip;",
+        	en : "You wonder what kind of trash could have made such a transformation&hellip;",
+        	eo : ""
         }
 	};
 	
--- WebContent/app/data/textes/stories/tuto/crypte/salle3-textes.js
+++ WebContent/app/data/textes/stories/tuto/crypte/salle3-textes.js
@@ -2,41 +2,50 @@
 define([], function(){
 	var data = {
         "necroS3-texte-1" : {
-            fr : "Vous pénétrez dans une vieille salle en ruine.",
-            en : "You enter into an old, ruined room."
+        	fr : "Vous pénétrez dans une vieille salle en ruine.",
+        	en : "You enter an old ruined room.",
+        	eo : ""
         },
         "necroS3-texte-2" : {
-            fr : "Apparemment, elle servait de bibliothèque de fortune.",
-            en : "Apparently, it was used as a makeshift library."
+        	fr : "Apparemment, elle servait de bibliothèque de fortune.",
+        	en : "Apparently, it used as a makeshift library.",
+        	eo : ""
         },
         "necroS3-texte-3" : {
-            fr : "Vous découvrez de nombreux ouvrages et parchemins dispersés un peu partout sur le sol.",
-            en : "You will discover many books and scrolls scattered all over the ground."
+        	fr : "Vous découvrez de nombreux ouvrages et parchemins dispersés un peu partout sur le sol.",
+        	en : "You discover many books and scrolls scattered all over the ground.",
+        	eo : ""
         },
         "necroS3-texte-4" : {
-            fr : "Une porte permet d'accéder à une autre partie du bâtiment.",
-            en : "A door gives access to another part of the building."
+        	fr : "Une porte permet d'accéder à une autre partie du bâtiment.",
+        	en : "A door gives access to another part of the building.",
+        	eo : ""
         },
 
         "necroS3Fouille-texte-1" : {
-            fr : "Il semblerait qu'il s'agisse de traités sur la magie.",
-            en : "Looks like it's about drafts on magic."
+        	fr : "Il semblerait qu'il s'agisse de traités sur la magie.",
+        	en : "It looks like magic treatises.",
+        	eo : ""
         },
         "necroS3Fouille-texte-2" : {
-            fr : "Beaucoup sont dans une langue incompréhensible,",
-            en : "Many are in an incomprehensible language,"
+        	fr : "Beaucoup sont dans une langue incompréhensible,",
+        	en : "Many are in an incomprehensible language,",
+        	eo : ""
         },
         "necroS3Fouille-texte-3" : {
-            fr : "mais vous parvenez à comprendre certains mots.",
-            en : "but you can understand some words."
+        	fr : "mais vous parvenez à comprendre certains mots.",
+        	en : "but you succeed to understand some words.",
+        	eo : ""
         },
         "necroS3Fouille-texte-4" : {
-            fr : "Cela parle d'une fusion entre les éléments qui permet de libérer une <span key='soin'>aura réparatrice</span>&hellip;",
-            en : "It speaks of a fusion between elements that allows the release of a <span key='soin'>restorative aura</span>&hellip;"
+        	fr : "Cela parle d'une fusion entre les éléments qui permet de libérer une <span key='soin'>aura réparatrice</span>&hellip;",
+        	en : "It speaks about a fusion between elements that permits the release of a <span key='soin'>restorative aura</span>&hellip;",
+        	eo : ""
         },
         "necroS3Fouille-texte-5" : {
-            fr : "Vous vous relevez et vous dirigez vers la porte.",
-            en : "You get up and head for the door."
+        	fr : "Vous vous relevez et vous dirigez vers la porte.",
+        	en : "You get up and head for the door.",
+        	eo : ""
         }
 	};
 	
