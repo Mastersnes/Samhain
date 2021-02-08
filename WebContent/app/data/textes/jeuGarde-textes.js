67dba4bd181f34c62ec8aa578a7c61c6a8dde561
Mastersnes <steven.nesztler@gmail.com>
2021-02-08 14:30:04 +0100

    Ajout du texte de Lili et de la correction de jean luc

--- WebContent/app/data/textes/jeuGarde-textes.js
+++ WebContent/app/data/textes/jeuGarde-textes.js
@@ -7,12 +7,12 @@ define([], function(){
             "pokgard-debut" : {
             	fr : "Allons-y mon gars, j’espère que t'es prêt&nbsp;!",
             	en : "Let's go dude, I hope you're ready!",
-            	eo : "Ek ulo! Mi esperas, ke vi estas preta!"
+            	eo : "Ek! Ulo! Mi esperas, ke vi estas preta!"
             },
             "pokgard-win" : {
             	fr : "Bien joué&nbsp;! T'en referas bien une&nbsp;? Non&nbsp;?",
             	en : "Well played! You’re gonna do another one, don’t you?",
-            	eo : "Bone ludita! Vi refaros ankoraŭ ĝin alian? Ne?"
+            	eo : "Bonege! Vi ja faros alian ludpartion? Ĉu ne?"
             },
 
             /**
@@ -71,12 +71,12 @@ define([], function(){
             "abandonner-confirm" : {
             	fr : "Désirez-vous réellement abandonner la partie&nbsp;?",
             	en : "Do you really want to give up the game?",
-            	eo : "̈Ĉu vi vere volas rezigni la ludo?"
+            	eo : "Ĉu vi vere volas rezigni la ludon?"
             },
             "regles" : {
             	fr : "Voir les règles",
             	en : "See the rules",
-            	eo : "Vidi la reguloj"
+            	eo : "Vidi la regulojn"
             },
 
             /**
@@ -90,7 +90,7 @@ define([], function(){
             "player-begin" : {
             	fr : "Vous commencez la partie.</br><b>Misez</b> ou <b>passez votre tour</b>.",
             	en : "You begin the game.</br><b>Bet</b> or <b>pass your turn</b>.",
-            	eo : "Vi komencas la ludon.</br><b>Vetu</b> aŭ <b>pasigu vian vicon</b>."
+            	eo : "Vi komencas la ludon.</br><b>Vetu</b> aŭ <b>rezignu vian vicon</b>."
             },
             "garde-turn" : {
             	fr : "C'est le tour du garde.",
@@ -100,17 +100,17 @@ define([], function(){
             "player-turn" : {
             	fr : "C'est votre tour.</br><b>Misez</b> ou <b>passez votre tour</b>.",
             	en : "It’s your turn.</br><b>Bet</b> or <b>pass your turn</b>.",
-            	eo : "Tio estas via vico.</br><b>Vetu</b> aŭ <b>pasigu vian vicon</b>."
+            	eo : "Tio estas via vico.</br><b>Vetu</b> aŭ <b>rezignu vian vicon</b>."
             },
             "player-turn-2" : {
             	fr : "Vous pouvez maintenant <b>piocher</b> ou <b>utiliser une action</b>.",
             	en : "Now, you can <b>draw</b> or <b>use an action</b>.",
-            	eo : "Nun vi povas <b>eltiri karto</b> aŭ <b>uzi ago</b>."
+            	eo : "Nun vi povas <b>eltiri karton</b> aŭ <b>uzi agon</b>."
             },
             "player-impossible" : {
             	fr : "Vous n'avez plus assez pour <b>miser</b>.<br/>Veuillez <b>passer votre tour</b>.",
             	en : "You don’t have enough left to <b>bet</b>.<br/>Please, <b>pass your turn</b>.",
-            	eo : "Vi ne havas pli sufiĉe por <b>veti</b>.<br/>Bonvolu <b>pasigi via vico</b>."
+            	eo : "Vi ne plu havas sufiĉe por <b>veti</b>.<br/>Bonvolu <b>rezigni vian vicon</b>."
             },
             "garde-use" : {
             	fr : "Le garde utilise l'action <b>?</b>.",
@@ -130,22 +130,22 @@ define([], function(){
             "garde-non-mise" : {
             	fr : "Le garde passe son tour.",
             	en : "Guard passes his turn.",
-            	eo : "La gardisto pasigas lian vicon."
+            	eo : "La gardisto rezignas sian vicon."
             },
             "player-win" : {
             	fr : "Félicitations, vous raflez la mise&nbsp;!</br><b>Cliquez pour continuer</b>.",
             	en : "Congratulations, you take the pot!</br><b>Click to continue</b>.",
-            	eo : "Gratulon! Vi gajnas la veton!</br><b>Kliku por daŭri</b>."
+            	eo : "Gratulon! Vi gajnas la veton!</br><b>Kliku por daŭrigi</b>."
             },
             "player-lose" : {
             	fr : "Vous perdez de <b>?</b> points contre <b>?</b>&hellip;</br>Le garde rafle la mise.</br><b>Cliquez pour continuer</b>.",
             	en : "You lose up to <b>?</b> points against <b>?</b>&hellip;</br>Guard takes the pot.</br><b>Click to continue</b>.",
-            	eo : "Vi malgajnas da <b>?</b> poentoj kontraŭ <b>?</b>&hellip;</br>La gardisto gajnas la veton.</br><b>Kliku por daŭri</b>."
+            	eo : "Vi malgajnas da <b>?</b> poentoj kontraŭ <b>?</b>&hellip;</br>La gardisto gajnas la veton.</br><b>Kliku por daŭrigi</b>."
             },
             "match-nul" : {
             	fr : "Match nul&hellip;</br><b>Cliquez pour continuer</b>.",
             	en : "Draw&hellip;</br><b>Click to continue</b>.",
-            	eo : "Nula matĉo&hellip;</br><b>Kliku por daŭri</b>."
+            	eo : "Nula matĉo&hellip;</br><b>Kliku por daŭrigi</b>."
             },
 
             /**
@@ -154,17 +154,17 @@ define([], function(){
             "pokgard-voir-action" : {
             	fr : "Action&nbsp;: Voir la carte",
             	en : "Action: Look at the card",
-            	eo : "Ago: Vidi la karto"
+            	eo : "Ago: Vidi la karton"
             },
             "pokgard-voler-action" : {
             	fr : "Action&nbsp;: Voler la carte",
             	en : "Action: Steal the card",
-            	eo : "Ago: Ŝteli la karto"
+            	eo : "Ago: Ŝteli la karton"
             },
             "pokgard-detruire-action" : {
             	fr : "Action&nbsp;: Détruire la carte",
             	en : "Action: Destroy the card",
-            	eo : "Ago: Detrui la karto"
+            	eo : "Ago: Detrui la karton"
             },
 
             /**
@@ -173,43 +173,43 @@ define([], function(){
             "pokgard-tuto-0" : {
             	fr : "Tu vas voir, les règles sont très simples.</br><b>Clique ici</b>, je vais t'expliquer&hellip;",
             	en : "You’re gonna see, rules are very simple.</br><b>Click here</b>, I’m going to explain it to you&hellip;",
-            	eo : "Vi estas vidonta, la reguloj estas tre simplaj.</br><b>Kliku ĉi tie</b>, mi estas klarigonta vin&hellip;"
+            	eo : "Vi estas vidonta, la reguloj estas tre simplaj.</br><b>Kliku ĉi tie</b>, mi estas klarigonta al vi&hellip;"
             },
             "pokgard-tuto-1" : {
             	fr : "Normalement, le premier joueur est <b>tiré au sort</b>.</br>Mais pour cette partie, je te laisse <b>commencer</b>.",
             	en : "Usually, the first player is <b>randomly chosen</b>.</br>But for this game, I let you <b>begin</b>.",
-            	eo : "Kutime, la unua ludanto estas <b>lotita</b>.</br>Sed pri ĉi tio ludo, mi lasas <b>komenci</b> vin."
+            	eo : "Kutime, la unua ludanto estas <b>lotita</b>.</br>Sed pri ĉi tiu ludo, mi lasas vin <b>komenci</b>."
             },
             "pokgard-tuto-2" : {
             	fr : "Pour ce <b>premier tour</b>, la <b>mise</b> initiale est de <b>100po</b>.</br>Elle <b>augmentera</b> ensuite tout au long de la partie.",
             	en : "For this <b>first turn</b>, the starting <b>bet</b> is <b>100gold</b>.</br>Then, it’ll <b>increase</b> during the game.",
-            	eo : "Pri ĉi tio <b>unua vico</b>, la eka <b>veto</b> estas da <b>100oro</b>.</br>Poste, ĝi <b>pliiĝos</b> dum la ludo."
+            	eo : "Pri ĉi tiu <b>unua vico</b>, la eka <b>veto</b> estas da <b>100oro</b>.</br>Poste, ĝi <b>pliiĝos</b> dum la ludo."
             },
             "pokgard-tuto-3" : {
             	fr : "Tu es obligé de <b>miser</b> pour pouvoir jouer.</br>Si tu ne peux pas, il te faudra <b>passer ton tour</b>.",
             	en : "You must <b>bet</b> to be able to play.</br>If you can’t, you’ll have to <b>pass your turn</b>.",
-            	eo : "Vi estas devigata <b>veti</b> por povi ludi.</br>Se vi ne povas, vi devos <b>pasigi vian vicon</b>."
+            	eo : "Vi estas devigata <b>veti</b> por rajti ludi.</br>Se vi ne povas, vi devos <b>rezigni vian vicon</b>."
             },
             "pokgard-tuto-4" : {
             	fr : "<b>Clique</b> sur <b>'Miser'</b> pour continuer.",
             	en : "<b>Click</b> on <b>'Bet'</b> to continue.",
-            	eo : "<b>Kliku</b> sur <b>'Veti'</b> por daŭri."
+            	eo : "<b>Kliku</b> sur <b>'Veti'</b> por daŭrigi."
             },
 
             "pokgard-tuto-5" : {
             	fr : "Bien joué&nbsp;!</br>Maintenant que tu as <b>misé</b>, trois choix s'offrent à toi.",
             	en : "Well played!</br>Now that you have <b>bet</b>, you got three choices.",
-            	eo : "Bone ludita!</br>Nun ke vi estas <b>vetinta</b>, vi havas tri elektojn."
+            	eo : "Bonege!</br>Nun ke vi estas <b>vetinta</b>, vi havas tri elektojn."
             },
             "pokgard-tuto-6" : {
             	fr : "Tu peux <b>miser</b> de nouveau et ainsi <b>augmenter la mise</b> que doivent verser les joueurs <b>à chaque tour</b>.",
             	en : "You’re able <b>to bet</b> again and thus <b>increase the bet</b> that players have to pay <b>each turn</b>.",
-            	eo : "Vi povas <b>veti</b> denove kaj tiel <b>pliigi la veto</b> ke devas pagi la ludantoj dum <b>ĉiu vico</b>."
+            	eo : "Vi povas <b>veti</b> denove kaj tiel <b>pliigi la veton</b>, kiun devas pagi la ludantoj dum <b>ĉiu vico</b>."
             },
             "pokgard-tuto-7" : {
             	fr : "Tu peux <b>passer ton tour</b> et donc abandonner ton droit de jouer cette fois-ci.",
             	en : "You’re able to <b>pass your turn</b> and so give up your right to play this time.",
-            	eo : "Vi povas <b>pasigi vian vicon</b> kaj do rezigni vian rajton ludi ĉi tiun fojon."
+            	eo : "Vi povas <b>rezigni vian vicon</b> kaj do rezigni vian rajton ludi ĉi tiun fojon."
             },
             "pokgard-tuto-8" : {
             	fr : "Ou bien, tu peux <b>piocher</b> pour tenter d'obtenir des points.",
@@ -225,17 +225,18 @@ define([], function(){
             "pokgard-tuto-10" : {
             	fr : "Félicitations, tu as <b>pioché</b> ta première carte&nbsp;! Cela a automatiquement <b>passé ton tour</b>.",
             	en : "Congratulations, you <b>drew</b> your first card! This has automatically <b>past your turn</b>.",
-            	eo : "Gratulon! Vi estas <b>eltirinta</b> via unua karto! Tio aŭtomate <b>pasigis via vico</b>."
+            	eo : "Gratulon! Vi estas <b>eltirinta</b> vian unuan karton! Tio aŭtomate <b>nuligis via vico</b>."
             },
             "pokgard-tuto-11" : {
             	fr : "J'en ai donc profité pour <b>jouer</b> et <b>piocher</b> également.",
             	en : "So I took the opportunity to <b>play</b> and <b>draw</b> too.",
-            	eo : "Do, ankaŭ mi estas profitinta por <b>ludi</b> kaj <b>eltiri karto</b>."
+            	eo : "Do, ankaŭ mi estas profitinta por <b>ludi</b> kaj <b>eltiri karton</b>."
+            	      Do, ankau mi povis ludi kaj eltiri karton.
             },
             "pokgard-tuto-12" : {
             	fr : "Il est possible de <b>piocher</b> deux types de cartes : <b>les points</b> et <b>les actions</b>.",
             	en : "It’s possible to draw two types of cards: <b>points</b> and <b>actions</b>.",
-            	eo : "Eblas eltiri du kartotipoj: <b>poentoj</b> kaj <b>agoj</b>."
+            	eo : "Eblas eltiri du kartotipojn: <b>poentojn</b> kaj <b>agojn</b>."
             },
             "pokgard-tuto-13" : {
             	fr : "La <b>pomme</b> vaut <b>1 point</b>,</br>Le <b>fromage</b> en vaut <b>2</b>,</br>Et la <b>potion</b> en vaut <b>3</b>.",
@@ -245,33 +246,33 @@ define([], function(){
             "pokgard-tuto-14" : {
             	fr : "Garde-les précieusement. Plus tu as de <b>points</b>, plus tu as de chance de <b>gagner la partie</b>&nbsp;!",
             	en : "Treasure them. The more <b>points</b> you have the more chance you have to <b>win the game</b>!",
-            	eo : "Konservu ilin altvalore. Pli vi havas <b>poentojn</b>, pli vi havas ŝancojn <b>gagni la ludon</b>!"
+            	eo : "Zorge konservu ilin. Ju pli vi havas <b>poentojn</b>, des pli vi havas ŝancojn <b>gagni la ludon</b>!"
             },
             "pokgard-tuto-15" : {
             	fr : "Les <b>actions</b> permettent de <b>voir</b>, <b>voler</b> ou même <b>détruire</b> les cartes adverses.",
             	en : "The <b>actions</b> allow you to <b>see</b>, <b>steal</b> or <b>destroy</b> adverse cards.",
-            	eo : "La <b>agoj</b> ebligas <b>viri</b>, <b>ŝteli</b> aŭ <b>detrui</b> adversajn kartojn."
+            	eo : "La <b>agoj</b> ebligas <b>vidi</b>, <b>ŝteli</b> aŭ <b>detrui</b> adversajn kartojn."
             },
             "pokgard-tuto-16" : {
-            	fr : "Tu en as justement une. <b>Clique</b> dessus et <b>utilise</b> là sur ma carte&nbsp;!",
+            	fr : "Tu en as justement une. <b>Clique</b> dessus et <b>utilise</b> la sur ma carte&nbsp;!",
             	en : "Actually, you got one. <b>Click</b> on it and <b>use</b> it on my card!",
-            	eo : "Nuntempe, vi havas unu. <b>Kliku</b> sur ĝin kaj <b>uzu</b> ĝin sur mia karto!"
+            	eo : "Ĝuste nun, vi havas unu. <b>Kliku</b> sur ĝin kaj <b>uzu</b> ĝin sur mia karto!"
             },
 
             "pokgard-tuto-17" : {
             	fr : "Bien joué&nbsp;!</br>Les <b>actions</b> te permettront de renverser la situation et d'amasser <b>plus de points</b>.",
             	en : "Well played! The <b>actions</b> will allow you to reverse the situation and to amass <b>more points</b>.",
-            	eo : "Bone ludita! La <b>agoj</b> ebligos vin renversi la situacion kaj amasigi <b>pli poentojn</b>."
+            	eo : "Bonege! La <b>agoj</b> ebligos vin inversigi la situacion kaj amasigi <b>pliajn poentojn</b>."
             },
             "pokgard-tuto-18" : {
             	fr : "Bien, maintenant, c'est à toi de <b>jouer</b>&nbsp;!</br>N'oublie pas : ne <b>mise</b> jamais plus que ce que tu possèdes&nbsp;!",
             	en : "Good, now it’s your <b>turn</b>!</br>Don’t forget: Never <b>bet</b> more than you possess!",
-            	eo : "Bone, nun tio estas via <b>vico</b>!</br>Ne forgesu: neniam <b>metu</b> pli ol vi posedas!"
+            	eo : "Bone, nun tio estas via <b>vico</b>!</br>Ne forgesu: neniam <b>vetu</b> pli ol vi posedas!"
             },
             "pokgard-tuto-18-2" : {
             	fr : "Bien, maintenant, reprenons notre <b>partie</b>&nbsp;!</br>N'oublie pas : ne <b>mise</b> jamais plus que ce que tu possèdes&nbsp;!",
             	en : "Good, now let’s continue our <b>game</b>!</br>Don’t forget: Never <b>bet</b> more than you possess!",
-            	eo : "Bone, nun daŭras nian <b>ludon</b>!</br>Ne forgesu: neniam <b>metu</b> pli ol vi posedas!"
+            	eo : "Bone, nun daŭrigu nian <b>ludon</b>!</br>Ne forgesu: neniam <b>vetu</b> pli ol vi posedas!"
             }
 	};
 	
--- WebContent/app/data/textes/stories/tuto/debut-textes.js
+++ WebContent/app/data/textes/stories/tuto/debut-textes.js
@@ -4,87 +4,87 @@ define([], function(){
         "tuto-start-texte-1" : {
         	fr : "Une nouvelle journée se termine.",
         	en : "A new day ends.",
-        	eo : ""
+        	eo : "Nova tago finiĝas."
         },
         "tuto-start-texte-2" : {
         	fr : "La douceur de l'automne caresse les feuilles qui virevoltent aux alentours.",
         	en : "The autumn sweetness caresses the leaves that spin around.",
-        	eo : ""
+        	eo : "La dolĉeco de la aŭtuno karesas la foliojn, kiuj flirtas ĉirkaŭe."
         },
         "tuto-start-texte-3" : {
         	fr : "Ayant terminé votre dur labeur un peu plus tôt,",
         	en : "Having finished your hard work a little sooner,",
-        	eo : ""
+        	eo : "Fininte vian malfacilan laboron iom pli frue."
         },
         "tuto-start-texte-4" : {
         	fr : "Vous ramassez une <span key='pomme'>pomme</span> de votre panier et décidez de vous reposer au pied du géant feuillu.",
         	en : "You pick an <span key='pomme'>apple</span> from your basket and decide to rest at the foot of a leafy giant.",
-        	eo : ""
+        	eo : "Vi prenas <span key='pomme'>pomon</span> en via korbo kaj decidas ripozi vin piede de giganta arbo."
         },
 
         "repos2-texte-1" : {
         	fr : "Vous fermez les yeux pour mieux apprécier la brise.",
         	en : "You close your eyes to better appreciate the breeze.",
-        	eo : ""
+        	eo : "Vi malfermas viajn okulojn por pli bone ŝati la brizon."
         },
         "repos2-texte-2" : {
         	fr : "Au loin, il vous semble entendre des bruits de pattes&hellip;",
         	en : "In the distance, you seem to hear legs noises&hellip;",
-        	eo : ""
+        	eo : "Fore, ŝajnas al vi aŭdi bruojn de piedojn&hellip;"
         },
         "repos2-texte-3" : {
-        	fr : "Vous vous levez en sursaut, quand trois <span key='araignee'>araignées</span> vous sautent dessus&nbsp;!",
-        	en : "You get up in a jolt when three <span key='araignee'>spiders</span> jump on you!",
-        	eo : ""
+        	fr : "Vous vous levez en sursaut, quand deux <span key='araignee'>araignées</span> vous sautent dessus&nbsp;!",
+        	en : "You get up in a jolt when two <span key='araignee'>spiders</span> jump on you!",
+        	eo : "Vi leviĝas subite, kiam du <span key='araignee'>araneoj</span> saltas sur vi!"
         },
 
         "reposCombat-texte-1" : {
         	fr : "Après avoir défait vos adversaires à la force de vos <span key='poing'>poings</span>,",
         	en : "After defeating your opponents with the strength of your <span key='poing'>fist</span>,",
-        	eo : ""
+        	eo : "Post venkinta viajn kontraŭulojn per la forto de viaj <span key='poing'>pugnoj</span>,"
         },
         "reposCombat-texte-2" : {
         	fr : "Vous faites une pause pour reprendre vos esprits.",
         	en : "You take a break to get your breath.",
-        	eo : ""
+        	eo : "Vi faras paŭzo por iom spiri."
         },
 
         "reposFouilleFail-texte-1" : {
         	fr : "Vous en fouillez une, mais ne trouvez que des entrailles&hellip; Berk&nbsp;!",
         	en : "You search one but only find guts&hellip; Yuck!",
-        	eo : ""
+        	eo : "Vi priserĉas unu, sed nur trovas viscerojn&hellip; Fi!"
         },
         "reposFouilleFail-texte-2" : {
         	fr : "J’espère que ce n'est pas ce que vous cherchiez&hellip; si&nbsp;?",
         	en : "I hope that's not what you’re looking for&hellip; is it?",
-        	eo : ""
+        	eo : "Mi esperas, ke tio ne estas kion vi serĉus&hellip; Ĉu?"
         },
 
         "reposFouilleSuccess-texte-1" : {
         	fr : "En remuant les entrailles, vous découvrez une belle <span key='pomme'>pomme</span>&nbsp;!",
         	en : "Stirring the guts you discover a beautiful <span key='pomme'>apple</span>!",
-        	eo : ""
+        	eo : "Movante la viscerojn, vi trovas belan <span key='pomme'>pomon</span>!"
         },
         "reposFouilleSuccess-texte-2" : {
         	fr : "Espérons que le rouge provienne bien de sa maturité&hellip;",
         	en : "Well, hope that the red comes from its maturity&hellip;",
-        	eo : ""
+        	eo : "Espereble la ruĝa devenas bone de sia matureco&hellip;"
         },
 
         "reposExamine-texte-1" : {
         	fr : "En vous penchant, vous remarquez que les traces de vos adversaires remontent vers une caverne non loin.",
         	en : "By stooping, you notice that your opponents' tracks go back to a nearby cave.",
-        	eo : ""
+        	eo : "Kliniĝante, vi rimarkas, ke la spuroj de viaj kontraŭuloj iras al kaverno proksima."
         },
         "reposExamine-texte-2" : {
         	fr : "On peut clairement y entendre grouiller divers insectes.",
         	en : "You can clearly hear various insects swarming around.",
-        	eo : ""
+        	eo : "Ni povas aŭdi klare svarmi diversajn insectojn."
         },
         "reposExamine-texte-3" : {
         	fr : "On se lance&nbsp;?",
         	en : "Let’s go?",
-        	eo : ""
+        	eo : "Ek!"
         }
 	};
 	
