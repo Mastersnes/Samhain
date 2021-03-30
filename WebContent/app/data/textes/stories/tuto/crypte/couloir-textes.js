c412f11c65c8a4639abb16edbc854514375a7bcb
Mastersnes <steven.nesztler@gmail.com>
2021-03-30 11:14:15 +0200

    Correction de la traduction et ajout des nouveaux textes de Lili

--- WebContent/app/data/textes/stories/tuto/crypte/couloir-textes.js
+++ WebContent/app/data/textes/stories/tuto/crypte/couloir-textes.js
@@ -4,75 +4,75 @@ define([], function(){
 	    "necroCouloir-texte-1" : {
         	fr : "Vous entrez dans un long couloir éclairé par de nombreuses torches.",
         	en : "You enter a long corridor lit by many torches.",
-        	eo : ""
+        	eo : "Vi eniras en longan koridoron lumigitan per multaj torĉoj."
         },
         "necroCouloir-texte-2" : {
         	fr : "Une ambiance calme et presque conviviale vous invite à avancer&hellip;",
         	en : "A calm and almost convivial atmosphere invites you to move forward&hellip;",
-        	eo : ""
+        	eo : "Trankvila kaj preskaŭ afabla etoso invitas vin antaŭeniri&hellip;"
         },
         "necroCouloir-texte-3" : {
         	fr : "Une grande porte de chêne vous fait face et deux portes plus petites",
         	en : "A large oak door faces you and two smaller doors",
-        	eo : ""
+        	eo : "Granda pordo el kverko frontas al vi kaj du pordoj pli malgrandaj"
         },
         "necroCouloir-texte-4" : {
         	fr : "se trouvent de chaque côté du couloir&hellip;",
         	en : "are on either side of the hallway&hellip;",
-        	eo : ""
+        	eo : "estas ambaŭflanke de la koridoro&hellip;"
         },
 
         "necroSalleFerme-texte-1" : {
         	fr : "Vous approchez de la porte, mais remarquez qu'elle ne possède ni poignée ni serrure&hellip;",
         	en : "You approach the door, but notice that it has no handle nor lock&hellip;",
-        	eo : ""
+        	eo : "Vi alproksimiĝas al la pordo, sed rimarkas, ke ĝi ne havas nek klinkon nek seruron&hellip;"
         },
         "necroSalleFerme-texte-2" : {
         	fr : "Pas très pratique tout de même.",
         	en : "Well, It’s not very useful.",
-        	eo : ""
+        	eo : "Ne tre praktika."
         },
         "necroSalleFerme-texte-3" : {
         	fr : "Vous essayez de forcer un peu pour l'ouvrir, mais impossible de la faire bouger.",
         	en : "You try to force a little to open it, but you can't make it move.",
-        	eo : ""
+        	eo : "Vi provas iom fortiri por malfermi ĝin, sed neeblas movi ĝin."
         },
 
         "necroS4-texte-1" : {
         	fr : "Vous entrez dans une pièce aux allures de prison.",
         	en : "You enter a room looking as a prison.",
-        	eo : ""
+        	eo : "Vi eniras en ĉambron, kiu aspektas kiel malliberejo."
         },
         "necroS4-texte-2" : {
         	fr : "De nombreux enfants sont agglutinés dans un coin.",
         	en : "Many children are clustered in a corner.",
-        	eo : ""
+        	eo : "Multaj infanoj estas aglutinitaj en angulo."
         },
         "necroS4-texte-3" : {
         	fr : "Lorsqu'ils s'enfuient à votre demande, l'un d'eux vous tend une <span key='potionSante'>fiole rouge</span>.",
-        	en : "As they run away at your request, one of them hands you a <span key='potionSante'>red flask</span>.",
-        	eo : ""
+        	en : "As they run away at your request, one of them gives you a <span key='potionSante'>red flask</span>.",
+        	eo : "Dum ili forkuras laŭ via peto, unu el ili donas al vi <span key='potionSante'>ruĝan pocion</span>."
         },
         "necroS4-texte-4" : {
         	fr : "Ça peut toujours aider&hellip;",
         	en : "It can always be helpful&hellip;",
-        	eo : ""
+        	eo : "Tio povos helpi vin&hellip;"
         },
 
         "necroS5-texte-1" : {
         	fr : "Vous entrez dans une belle bibliothèque pleine de livres anciens.",
         	en : "You enter a beautiful library full of old books.",
-        	eo : ""
+        	eo : "Vi eniras en belan bibliotekon plenan de malnovaj libroj."
         },
         "necroS5-texte-2" : {
         	fr : "Vous en lisez quelques-uns qui vous apprennent qu'il est possible,",
         	en : "You read a few of them, learning you that it is possible,",
-        	eo : ""
+        	eo : "Vi legas kelkajn, kiuj sciigas al vi, ke eblas,"
         },
         "necroS5-texte-3" : {
         	fr : "de faire sortir de l'énergie de son corps afin de <span key='guerison'>guérir</span> toute maladie.",
         	en : "to take energy out of your body in order to <span key='guerison'>cure</span> any disease.",
-        	eo : ""
+        	eo : "eligi energion el sia korpo por <span key='guerison'>kuraci</span> tutan malsanon."
         }
 	};
 	
--- WebContent/app/data/textes/stories/tuto/crypte/grandeSalle-textes.js
+++ WebContent/app/data/textes/stories/tuto/crypte/grandeSalle-textes.js
@@ -4,274 +4,274 @@ define([], function(){
         "necroGSFight-texte-1" : {
         	fr : "Vous pénétrez dans une très grande crypte.",
         	en : "You enter a very large crypt.",
-        	eo : ""
+        	eo : "Vi eniras en tre grandan kripton."
         },
         "necroGSFight-texte-2" : {
         	fr : "À votre arrivée, le gardien des lieux, une <span key='grosseGoule'>grosse goule</span>, se dirige vers vous.",
-        	en : "As your arrive, the place keeper, a <span key='grosseGoule'>big ghoul</span> is heading for you.",
-        	eo : ""
+        	en : "As your arrive, the place keeper, a <span key='grosseGoule'>big ghoul</span>, is heading for you.",
+        	eo : "Je via alveno, la gardisto de la loko, <span key='grosseGoule'>gulego</span>, alproksimiĝas al vi."
         },
         "necroGSFight-texte-3" : {
         	fr : "Sur le côté, deux <span key='squelette'>squelettes</span> sortent lentement de leurs cercueils&hellip;",
         	en : "On the side, two <span key='squelette'>skeletons</span> slowly come out of their coffins&hellip;",
-        	eo : ""
+        	eo : "Flanke, du <span key='squelette'>skeletoj</span> malrapide eliras el iliaj ĉerkoj&hellip;"
         },
 
         "necroGSFight2-texte-1" : {
         	fr : "Vous vous relevez péniblement après ce difficile combat.",
         	en : "You get up painfully after this difficult fight.",
-        	eo : ""
+        	eo : "Vi pene ekstaras post ĉi tiu malfacila batalo."
         },
         "necroGSFight2-texte-2" : {
         	fr : "Encore sous le choc, vous observez la pièce autour de vous&hellip;",
         	en : "Still in shock, you watch the room around you&hellip;",
-        	eo : ""
+        	eo : "Ankoraŭ ŝokita, vi observas la ĉambron ĉirkaŭ vi&hellip;"
         },
 
         "necroGSRoiFight-texte-1" : {
         	fr : "Le <span key='roiAraignee'>roi araignée</span> est couché au centre, inconscient, une <span key='grosseGoule'>grosse goule</span> morte dans la gueule&hellip;",
         	en : "The <span key='roiAraignee'>king of spiders</span> lies unconscious in the middle with a <span key='grosseGoule'>big dead ghoul</span> in his maw&hellip;",
-        	eo : ""
+        	eo : "La <span key='roiAraignee'>reĝo de la araneoj</span> kuŝas en la mezo, senkonscia, <span key='grosseGoule'>morta gulego</span> en la faŭko&hellip;"
         },
 
         "necroGSRoiFight2-texte-1" : {
         	fr : "Vous vous relevez péniblement après ce difficile combat et vous dirigez vers votre ami.",
         	en : "You get up up painfully after this difficult fight and you head for your friend.",
-        	eo : ""
+        	eo : "Vi pene ekstaras post ĉi tiu malfacila batalo kaj vi alproksimiĝas al via amiko."
         },
         "necroGSRoiFight2-texte-2" : {
         	fr : "Le roi, à l'agonie, vous exprime son dernier souhait&nbsp;:",
         	en : "The king, dying, expresses his last wish to you:",
-        	eo : ""
+        	eo : "La reĝo, agonianta, esprimas al vi sian lastan deziron:"
         },
         "necroGSRoiFight2-texte-3" : {
         	fr : "&laquo;&nbsp;Merci, noble aventurier. Je me meurs&hellip; Mais vous, vous pouvez encore sauver votre espèce.",
         	en : "&laquo;&nbsp;Thank you, noble adventurer. I'm dying&hellip; But you, you can still save your species.",
-        	eo : ""
+        	eo : "&laquo;&nbsp;Dankon, nobla aventurulo. Mi mortiĝas&hellip; Sed vi, vi povas ankoraŭ savi vian specon."
         },
         "necroGSRoiFight2-texte-4" : {
         	fr : "Vengez-nous, combattez pour l'araignée&nbsp;!&nbsp;&raquo;",
-        	en : "Avenger us, fight for the spider&nbsp;!&nbsp;&raquo;",
-        	eo : ""
+        	en : "Avenger us, fight for the spider!&nbsp;&raquo;",
+        	eo : "Venĝu nin, batalu por la araneo!&nbsp;&raquo;"
         },
         "necroGSRoiFight2-texte-5" : {
         	fr : "Avant de mourir, le roi s'arrache un <span key='crocRoi'>croc</span> et le jette à vos pieds.",
         	en : "Before he dies, the king tears a <span key='crocRoi'>fang</span> off and throws it at your feet.",
-        	eo : ""
+        	eo : "Antaŭ ol morti, la reĝo eltiras <span key='crocRoi'>dentegon</span> kaj ĵetas ĝin ĉe viaj piedoj."
         },
 
         "necroGSFight3Oui-texte-1" : {
         	fr : "Vous prenez l'arme en souvenir de votre ami.",
         	en : "You take this weapon as a souvenir of your friend.",
-        	eo : ""
+        	eo : "Vi prenas la armilo memore al via amiko."
         },
         "necroGSFight3Oui-texte-2" : {
         	fr : "Elle fera certainement une alliée précieuse&nbsp;!",
         	en : "It will certainly be a precious ally!",
-        	eo : ""
+        	eo : "Ĝi certe estos granda aliancano!"
         },
 
         "necroGSFight3Non-texte-1" : {
         	fr : "Vous laissez l'arme devant votre ami.",
         	en : "You leave the fang in front of your friend.",
-        	eo : ""
+        	eo : "Vi lasas la armilo antaŭ via amiko."
         },
         "necroGSFight3Non-texte-2" : {
         	fr : "Espérant qu'elle l'accompagne dans l'au-delà.",
         	en : "Hoping that it will accompany him to the afterlife.",
-        	eo : ""
+        	eo : "Esperante, ke ĝi akompanos lin en la transmondo."
         },
 
         "necroGS-texte-1" : {
         	fr : "Vous vous trouvez dans une gigantesque salle aux allures de crypte.",
         	en : "You are in a huge room looking as a crypt.",
-        	eo : ""
+        	eo : "Vi estas en giganta ĉambro, kiu aspektas kiel kripto."
         },
         "necroGS-texte-2" : {
         	fr : "Quelques cercueils sont éparpillés un peu partout.",
         	en : "Some coffins are scattered all over the place.",
-        	eo : ""
+        	eo : "Kelkaj ĉerkoj estas disigitaj ĉie."
         },
         "necroGS-texte-3" : {
-        	fr : "Vous remarquez trois portes : à gauche, en face et à droite&hellip;",
+        	fr : "Vous remarquez trois portes&nbsp;: à gauche, en face et à droite&hellip;",
         	en : "You notice three doors: on the left, in front of you and on the right&hellip;",
-        	eo : ""
+        	eo : "Vi rimarkas tri pordojn: maldekstre, fronte kaj dekstret&hellip;"
         },
 
         "necroGSFouilleCercueil-texte-1" : {
         	fr : "Avec dégoût, vous fouillez les cercueils.",
         	en : "With disgust, you search the coffins.",
-        	eo : ""
+        	eo : "Malinkline, vi priserĉas la ĉerkojn."
         },
         "necroGSFouilleCercueil-texte-2" : {
         	fr : "Les deux premiers sont vides, mais dans le troisiéme,",
         	en : "The first two are empty, but in the third,",
-        	eo : ""
+        	eo : "La du unuaj estas malplenaj, sed en la tria,"
         },
         "necroGSFouilleCercueil-texte-3" : {
         	fr : "vous apercevez une lueur jaune sous un tas d'os&hellip;",
         	en : "you notice a yellow glow under a pile of bones&hellip;",
-        	eo : ""
+        	eo : "vi ekvidas flavan lumon sub amaso de ostoj&hellip;"
         },
 
         "necroGSFouilleCercueil2-texte-1" : {
         	fr : "Vous saisissez un authentique <span key='elixir'>élixir</span>&nbsp;!",
         	en : "You grasp an authentic <span key='elixir'>elixir</span>!",
-        	eo : ""
+        	eo : "Vi prenas aŭtentikan <span key='elixir'>eliksiron/span>!"
         },
         "necroGSFouilleCercueil2-texte-2" : {
         	fr : "Malheureusement, vous n'êtes pas le seul sur le coup&hellip;",
         	en : "Unfortunately, you're not the only one on the case&hellip;",
-        	eo : ""
+        	eo : "Bedaŭrinde, vi ne estas sola en la afero."
         },
         "necroGSFouilleCercueil2-texte-3" : {
         	fr : "Une main osseuse vous tient fermement le poignet.",
         	en : "A bony hand firmly holds your wrist.",
-        	eo : ""
+        	eo : "Osta mano firme tenas vian manartikon."
         },
         "necroGSFouilleCercueil2-texte-4" : {
         	fr : "Vous vous écartez brusquement, emportant la main avec vous,",
         	en : "You suddenly step aside, taking the hand with you,",
-        	eo : ""
+        	eo : "Vi subite malantaŭas, forprenante la nano kun vi,"
         },
         "necroGSFouilleCercueil2-texte-5" : {
         	fr : "pendant qu'un <span key='squelette'>squelette manchot</span> sort difficilement de son cercueil&hellip;",
         	en : "while a <span key='squelette'>one-armed skeleton</span> hardly comes out of its coffin&hellip;",
-        	eo : ""
+        	eo : "dum <span key='squelette'>unubraka skeleto</span> malfacile eliras el lia ĉerko&hellip;"
         },
 
         "necroGSFouilleCercueil3-texte-1" : {
         	fr : "Vous vous débarrassez facilement de votre adversaire.",
         	en : "You easily get rid of your opponent.",
-        	eo : ""
+        	eo : "Vi facile liberigas de via kontraŭulon."
         },
         "necroGSFouilleCercueil3-texte-2" : {
         	fr : "Il est temps de continuer votre aventure&nbsp;!",
         	en : "It's time to continue your adventure!",
-        	eo : ""
+        	eo : "Estas tempo de daŭrigi vian aventuron!"
         },
 
         "necroGSFouilleSalle-texte-1" : {
         	fr : "Il s'agit d'une grande salle ressemblant à s'y méprendre à une crypte.",
         	en : "It’s a large room that looks like a crypt.",
-        	eo : ""
+        	eo : "Ĝ estas granda ĉambro, aspektante kiel kripto."
         },
         "necroGSFouilleSalle-texte-2" : {
         	fr : "Les murs sont suintants d'humidité et un froid intense la parcourt.",
         	en : "The walls oozes and an intense cold goes all over it.",
-        	eo : ""
+        	eo : "La muroj ŝvitas per humido kaj intensa malvarmo trairas ĝin."
         },
         "necroGSFouilleSalle-texte-3" : {
         	fr : "Deux torches se trouvent accrochées sur le mur du fond&hellip;",
         	en : "Two torches are hung up on the back wall&hellip;",
-        	eo : ""
+        	eo : "Du torĉoj estas kroĉitaj sur la muro fonde&hellip;"
         },
 
         "necroGSTorcheG-texte-1" : {
         	fr : "Vous vous approchez de la torche sur le mur de gauche.",
         	en : "You approach the torch on the left wall.",
-        	eo : ""
+        	eo : "Vi alproksimiĝas al la torĉo sur la maldekstra muro."
         },
         "necroGSTorcheG-texte-2" : {
         	fr : "Celle-ci est éteinte.",
         	en : "This one is off.",
-        	eo : ""
+        	eo : "Ĉi tiu estas malŝaltita."
         },
         "necroGSTorcheG-texte-3" : {
         	fr : "Vous ne trouvez pas qu'il fait sombre&nbsp;? Pourquoi ne pas l'allumer&nbsp;?",
         	en : "Don't you think it's dark? Why don't you light it?",
-        	eo : ""
+        	eo : "Ĉu vi pensas, ke estas mallume? Kial ne ŝalti ĝin?"
         },
 
         "necroGSTorcheD-texte-1" : {
         	fr : "Vous vous approchez de la torche sur le mur de droite.",
         	en : "You approach the torch on the right wall.",
-        	eo : ""
+        	eo : "Vi alproksimiĝas al la torĉo sur la dekstra muro."
         },
         "necroGSTorcheD-texte-2" : {
         	fr : "Vous ne trouvez pas qu'il fait froid&nbsp;? Pourquoi ne pas se réchauffer un peu&nbsp;?",
         	en : "Don't you think it's cold? Why don't we just warm up a little?",
-        	eo : ""
+        	eo : "Ĉu vi pensas, ke estas malvarme? Kial ne iomete varmiĝi?"
         },
 
         "necroGSTorcheOK-texte-1" : {
         	fr : "Vous allumez consciencieusement la torche, en prenant soin de ne pas vous brûler.",
         	en : "You light the torch conscientiously, taking care not to burn yourself.",
-        	eo : ""
+        	eo : "Vi konscience ekbruligas la torĉo, zorgante ne bruligi vin."
         },
         "necroGSTorcheOK-texte-2" : {
         	fr : "Soudain, vous entendez un système d'engrenages s'enclencher&hellip;",
         	en : "Suddenly, you hear a gear system&hellip;",
-        	eo : ""
+        	eo : "Subite, vi aŭdas dentradojn sistemon&hellip;"
         },
         "necroGSTorcheOK-texte-3" : {
         	fr : "Il semblerait qu'une porte se soit ouverte quelque part.",
         	en : "It looks like a door has opened somewhere.",
-        	eo : ""
+        	eo : "Ŝajnas, ke pordo estus malfermiĝis ie."
         },
 
         "necroGSTorcheKO-texte-1" : {
         	fr : "Vous lancez votre plus belle <span key='bouleFeu'>boule de feu</span>&nbsp;!",
         	en : "You throw your most beautiful <span key='bouleFeu'>fireball</span>!",
-        	eo : ""
+        	eo : "Vi lansas vian plej belan <span key='bouleFeu'>fajrobulon</span>!"
         },
         "necroGSTorcheKO-texte-2" : {
         	fr : "Celle-ci éclate violemment sur le mur et explose la torche.",
         	en : "This one bursts violently on the wall and explodes the torch.",
-        	eo : ""
+        	eo : "̈Ĉi tiu forte eksplodas sur la muro kaj eksplodigas la torĉon."
         },
         "necroGSTorcheKO-texte-3" : {
         	fr : "Il ne reste plus qu'une vieille tige en souvenir.",
         	en : "There's only one old rod left as a souvenir.",
-        	eo : ""
+        	eo : "Nur restas malnova stangeto."
         },
         "necroGSTorcheKO-texte-4" : {
         	fr : "R.I.P la torche&hellip;",
         	en : "R.I.P the torch&hellip;",
-        	eo : ""
+        	eo : "Adiaŭ la torĉo&hellip;"
         },
 
         "necroGSTorcheAllume-texte-1" : {
         	fr : "La torche brille de mille feux&nbsp;!",
         	en : "The torch shines brightly!",
-        	eo : ""
+        	eo : "La torĉo intense brilas!"
         },
         "necroGSTorcheAllume-texte-2" : {
         	fr : "Ne la regardez pas trop longtemps, ça pourrait brûler&nbsp;!",
         	en : "Don't look at it too long, it could burn!",
-        	eo : ""
+        	eo : "Ne rigardu ĝin tro longe, tio povus bruli!"
         },
 
         "necroGSTorcheExplose-texte-1" : {
         	fr : "Une vieille tige carbonisée se dresse fièrement sur le mur.",
         	en : "An old carbonized rod stands proudly on the wall.",
-        	eo : ""
+        	eo : "Malnova bruligita stangeto fiere staras sur la muro."
         },
         "necroGSTorcheExplose-texte-2" : {
         	fr : "Elle n'avait pourtant rien fait de mal&hellip;",
         	en : "It had done nothing wrong yet&hellip;",
-        	eo : ""
+        	eo : "Tamen, ĝi faris nenion malbonan&hellip;"
         },
 
         "necroGSS2-texte-1" : {
         	fr : "Vous faites face à une petite porte fermée à clef.",
         	en : "You're facing a small locked door.",
-        	eo : ""
+        	eo : "Vi estas antaŭ malgranda ŝlosita pordo."
         },
         "necroGSS2-texte-2" : {
         	fr : "Elle semble très résistante&hellip;",
         	en : "It seems very resistant&hellip;",
-        	eo : ""
+        	eo : "Ŝajnas, ke ĝi estas tre rezista."
         },
         "necroGSS2-texte-3" : {
         	fr : "En tendant l'oreille, vous entendez des cris,",
         	en : "When you listen carefully, you hear some screams,",
-        	eo : ""
+        	eo : "Aŭskultante pli bone, vi aŭdas kriojn,"
         },
         "necroGSS2-texte-4" : {
         	fr : "comme si une personne essayait de se débattre.",
         	en : "like someone trying to struggle.",
-        	eo : ""
+        	eo : "kvazaŭ persono provas barakti."
         }
 	};
 	
